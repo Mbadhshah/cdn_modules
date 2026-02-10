0c8ee3e5a672982454555780aba976fa407c4953
Mbadhshah <muhammedbadhshah.cm@gmail.com>
2026-02-10 12:12:36 +0530

    Update magneticpnp.jsx

--- modules/pnp/magneticpnp.jsx
+++ modules/pnp/magneticpnp.jsx
@@ -1,1 +1,12 @@
-hi
+import React from 'react';
+import './magneticpnp.css';
+
+function MagneticPNP() {
+    return (
+        <div className="magnetic-container">
+            <h1>Hi</h1>
+        </div>
+    );
+}
+
+export default MagneticPNP;
